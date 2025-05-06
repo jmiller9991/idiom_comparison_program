91b78568ce88aa954ca046cf963f6047f22843a6
jmiller9991 <jacobmiller9991@gmail.com>
2025-05-05 22:29:57 -0400

    Data formatting complete

--- test_scores.py
+++ test_scores.py
@@ -13,7 +13,6 @@ def splitstr(sentence):
 # result should be in form of ['<String input>']
 def bertscore_subprocessor(string_to_mod):
     result = [string_to_mod]
-    print(string_to_mod)
     return result
 
 # sub-parser for BLEU score
@@ -24,8 +23,35 @@ def bleuscore_subprocessor(string_to_mod):
 
 # get the csv file as input and break it down to numpy array
 def loadAndParse(file):
+    # import data
     data_frame_corpus = pd.read_csv(file)
-    return data_frame_corpus
+
+    # get language arrays from dataframe
+    spanish_data = data_frame_corpus['Spanish'].tolist()
+    english_data = data_frame_corpus['English (Metaphorical)'].tolist()
+
+    # loop through spanish data and make BERTScore data
+    spanish_data_bert = []
+    for i in spanish_data:
+        spanish_data_bert.append(bertscore_subprocessor(i))
+
+    # loop through english data and make BERTScore data
+    english_data_bert = []
+    for i in english_data:
+        english_data_bert.append(bertscore_subprocessor(i))
+
+    # loop through spanish data and make BLEU Score data
+    spanish_data_bleu = []
+    for i in spanish_data:
+        spanish_data_bleu.append(bleuscore_subprocessor(i))
+
+
+    # loop through english data and make BLEU Score data
+    english_data_bleu = []
+    for i in english_data:
+        english_data_bleu.append(bleuscore_subprocessor(i))
+
+    return spanish_data_bert, english_data_bert, spanish_data_bleu, english_data_bleu
 
 # get the BERTScore for English
 def perform_BERTScore(gen, ref, lang):
@@ -40,34 +66,39 @@ def perform_BLEUScore(gen, ref):
 # main to run the program
 def main():
     # load and parse the comma delimited corpus data as a csv file
-    LAP = loadAndParse('comma_delimited_data.csv')
+    es_ref_array_bert, en_ref_array_bert, es_ref_array_bleu, en_ref_array_bleu = loadAndParse('comma_delimited_data.csv')
+    print(es_ref_array_bert)
+    print(en_ref_array_bert)
+    print(es_ref_array_bleu)
+    print(en_ref_array_bleu)
+
 
 if __name__ == '__main__':
     main()
 
 
-    # BLEU test
-    # reference = splitstr('The quick brown fox jumps over the lazy dog')
-    # bad_candidate = splitstr('The quack brown fox jumps over the lazy dog')
-    # really_bad_candidate = splitstr('Bad Example')
-    #
-    # good_bleu = perform_BLEUScore(reference, reference)
-    # bad_bleu = perform_BLEUScore(bad_candidate, reference)
-    # really_bad_bleu = perform_BLEUScore(really_bad_candidate, reference)
-    #
-    # print(good_bleu)
-    # print(bad_bleu)
-    # print(really_bad_bleu)
+# BLEU test
+# reference = splitstr('The quick brown fox jumps over the lazy dog')
+# bad_candidate = splitstr('The quack brown fox jumps over the lazy dog')
+# really_bad_candidate = splitstr('Bad Example')
+#
+# good_bleu = perform_BLEUScore(reference, reference)
+# bad_bleu = perform_BLEUScore(bad_candidate, reference)
+# really_bad_bleu = perform_BLEUScore(really_bad_candidate, reference)
+#
+# print(good_bleu)
+# print(bad_bleu)
+# print(really_bad_bleu)
 
-    # BERTScore test
-    # really_bad_candidate = ['BAD EXAMPLE']
-    # bad_canidate = ['The quack brown fox jumps over the lazy dog']
-    # reference = ['The quick brown fox jumps over the lazy dog']
-    #
-    # good_p, good_r, good_f1 = perform_BERTScore(reference, reference, 'en')
-    # bad_p, bad_r, bad_f1 = perform_BERTScore(bad_canidate, reference, 'en')
-    # rbad_p, rbad_r, rbad_f1 = perform_BERTScore(really_bad_candidate, reference, 'en')
-    #
-    # print(f'Good test: P: {good_p.mean().item()}, R: {good_r.mean().item()}, F1: {good_f1.mean().item()}')
-    # print(f'Bad test: P: {bad_p.mean().item()}, R: {bad_r.mean().item()}, F1: {bad_f1.mean().item()}')
-    # print(f'Really Bad test: P: {rbad_p.mean().item()}, R: {rbad_r.mean().item()}, F1: {rbad_f1.mean().item()}')
+# BERTScore test
+# really_bad_candidate = ['BAD EXAMPLE']
+# bad_canidate = ['The quack brown fox jumps over the lazy dog']
+# reference = ['The quick brown fox jumps over the lazy dog']
+#
+# good_p, good_r, good_f1 = perform_BERTScore(reference, reference, 'en')
+# bad_p, bad_r, bad_f1 = perform_BERTScore(bad_canidate, reference, 'en')
+# rbad_p, rbad_r, rbad_f1 = perform_BERTScore(really_bad_candidate, reference, 'en')
+#
+# print(f'Good test: P: {good_p.mean().item()}, R: {good_r.mean().item()}, F1: {good_f1.mean().item()}')
+# print(f'Bad test: P: {bad_p.mean().item()}, R: {bad_r.mean().item()}, F1: {bad_f1.mean().item()}')
+# print(f'Really Bad test: P: {rbad_p.mean().item()}, R: {rbad_r.mean().item()}, F1: {rbad_f1.mean().item()}')
