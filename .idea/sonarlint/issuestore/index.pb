bf7e0e805f2bcd3bfe3ad926a7121b263382b4f0
jmiller9991 <jacobmiller9991@gmail.com>
2025-05-07 20:49:33 -0400

    Initial Commit and BLEU score and BERTScore calculations

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,9 @@ H
 comma_delimited_data.csv,8\9\899255e2872730de00c6609525567e934f9ce749
 >
 test_scores.py,3\9\39e4198aea5d21c32e7c61d6f145f8720e962ab9
+@
+requirements.txt,1\9\19359a61ae2446b51b549167b014da2fcf265768
+F
+token_hugging_face.txt,9\a\9a52a8e246200f81f2388bd2bd70e6be714466ca
+J
+training_data_collector.py,b\9\b90ecf15f96e3c300c19e2e9ca9de6bce8bf29ea
---
+++ training_data_collector.py
@@ -0,0 +1,67 @@
+from transformers import pipeline
+from huggingface_hub import login
+import sys
+import pandas as pd
+import numpy as np
+import time
+
+
+# read token from token_hugging_face.txt
+def token_grabber(possible_token=None):
+    token = ""
+
+    if possible_token is not None:
+        token = possible_token
+    else:
+        with open('token_hugging_face.txt', 'r') as f:
+            token = f.readline()
+
+    return token
+
+
+# loads the data as lists to translate and loop
+def load_data_for_translation(file):
+    data_frame_corpus = pd.read_csv(file)
+
+    # get language arrays from dataframe
+    spanish_data = data_frame_corpus['Spanish'].tolist()
+    english_data = data_frame_corpus['English (Metaphorical)'].tolist()
+
+    return spanish_data, english_data
+
+# this method gathers data from an AI, translating all values from corpus to language provided by user
+def gather_data_from_ai(translation_array, language, model, token):
+    result_array = []
+
+    login(token=token)
+
+    generator = pipeline('text-generation', model=model)
+
+    for i in translation_array:
+        question = f"Can you translate {i} into {language}?"
+        response = generator(question, max_new_tokens=50)
+
+        print(response[0]['generated_text'])
+
+        result_array.append(response[0]['generated_text'])
+
+    return result_array
+
+# converts the answer response to a csv file
+def convert_answers_array(answer_array):
+    print(answer_array)
+
+# main program execution
+def main():
+    if len(sys.argv) > 1:
+        token = token_grabber(sys.argv[1])
+    else:
+        token = token_grabber()
+
+    spanish_data, english_data = load_data_for_translation('comma_delimited_data.csv')
+    print(token)
+
+
+if __name__ == '__main__':
+    main()
+
